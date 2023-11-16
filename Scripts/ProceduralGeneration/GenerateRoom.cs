e1c6e490b70725ed0227a5034577eb1623ce632c
Lilian Labat <lilian.labat@epita.fr>
2023-11-16 16:39:40 +0100

    feat: first iteration of room generation

--- Scripts/ProceduralGeneration/GenerateRoom.cs
+++ Scripts/ProceduralGeneration/GenerateRoom.cs
@@ -4,25 +4,45 @@ using System.Diagnostics;
 
 public partial class GenerateRoom : Node
 {
-	public Resource[] prefabS1;
-
-	public void GenerateObjectsRoom()
+	public void GenerateObjectsRoom(PackedScene[] prefabS1, PackedScene[] prefabS2)
 	{
-		foreach(Node node in GetChildren())
+        Random rand = new Random();
+        foreach (Node node in GetChildren())
 		{
-            Debug.Print(node.GetScript().AsStringName());
-            if (node.GetScript().AsStringName() == "SpawnPointS1")
+            if (node.Name == "SpawnPointS1")
 			{
-                
+				Node tObj = prefabS1[rand.Next(prefabS1.Length)].Instantiate();
+				node.AddChild(tObj);
             }
-		}
+
+            if (node.Name == "SpawnPointS2")
+            {
+                Node tObj = prefabS2[rand.Next(prefabS2.Length)].Instantiate();
+                node.AddChild(tObj);
+            }
+        }
 	}
 
     public override void _Ready()
     {
-		prefabS1 = new Resource[1];
-		prefabS1[0] = ResourceLoader.Load("res://Scenes/Lilian/Template-Tiles/tile1.tscn");
+		Godot.Collections.Array s1collec = GetMeta("prefabS1").AsGodotArray();
+        PackedScene[] prefabS1 = new PackedScene[s1collec.Count];
 
-        GenerateObjectsRoom();
+        Godot.Collections.Array s2collec = GetMeta("prefabS2").AsGodotArray();
+        PackedScene[] prefabS2 = new PackedScene[s2collec.Count];
+
+        for (int i = 0; i < prefabS1.Length; i++)
+		{
+			prefabS1[i] = (PackedScene)s1collec[i];
+        }
+
+        for (int i = 0; i < prefabS2.Length; i++)
+        {
+            prefabS2[i] = (PackedScene)s2collec[i];
+        }
+
+        //Debug.Print(GetMeta("prefabS1").AsGodotArray().Count.ToString());
+
+        GenerateObjectsRoom(prefabS1, prefabS2);
     }
 }
