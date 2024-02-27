ab16eff980c5b0ae30a35ba374d5f88bab59c3f0
Guillaume Dufour <guillaume.dufour@epita.fr>
2024-02-27 11:56:57 +0100

    feat:ui finished ui

--- Scripts/Guillaume/Lobby_Script.cs
+++ Scripts/Guillaume/Lobby_Script.cs
@@ -1,15 +1,20 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class Lobby_Script : CanvasLayer
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GetNode<Button>("Custom/MarginContainer7/VBoxContainer/Add_Team_1").ButtonUp += (() => {PlayerJoin("pute!", 0);});
+		GetNode<Button>("Custom/MarginContainer6/VBoxContainer/Add_Team_2").ButtonUp += (() => {PlayerJoin("pute2!", 1);});
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void PlayerJoin(string playerName, uint team)
 	{
+		var packedscene = GD.Load<PackedScene>("res://Scenes/Guillaume/Lobby_Player_Template.tscn");
+		var player = packedscene.Instantiate<Control>();
+		GetNode<Control>(team == 0 ? "Custom/ScrollContainer/VBoxContainer" : "Custom/ScrollContainer2/VBoxContainer").AddChild(player);
 	}
 }
